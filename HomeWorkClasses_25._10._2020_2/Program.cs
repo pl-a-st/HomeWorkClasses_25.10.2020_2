c4e31a6e1607b1cf16d6b1e061af5b857df072e6
Максим Богащенко <pl_a_st@live.ru>
2020-11-07 16:39:13 +0300

    123

--- HomeWorkClasses_25._10._2020_2/Program.cs
+++ HomeWorkClasses_25._10._2020_2/Program.cs
@@ -6,7 +6,7 @@ namespace HomeWorkClasses_25._10._2020_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello  World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
