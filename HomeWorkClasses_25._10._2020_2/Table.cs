6ce45aa3b4439e5e75cd860a0eff6f74a2e94b5f
Максим Богащенко <pl_a_st@live.ru>
2020-11-07 18:33:07 +0300

    07/11/20 18:32

---
+++ HomeWorkClasses_25._10._2020_2/Table.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWorkClasses_25._10._2020_2
+{
+    class Table
+    {
+        public int Length;
+        public int Width;
+        public TableСoordinates TableСoordinates;
+    }
+}
---
+++ HomeWorkClasses_25._10._2020_2/TableСoordinates.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWorkClasses_25._10._2020_2
+{
+    public class TableСoordinates
+    {
+        public List<PartsTable> PartsTablesСoordinate;
+    }
+}
