6eb88b55641c8819e62fce7b690643a7439e452c
Максим Богащенко <pl_a_st@live.ru>
2020-11-07 16:37:06 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkClasses_25._10._2020_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWorkClasses_25._10._2020_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello  World!");
+        }
+    }
+}
